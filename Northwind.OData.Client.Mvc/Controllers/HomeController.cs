3ef224dc819de930b787cb6fd84ef8973f0a386b
Дмитрий Кулик <liamkukid@gmail.com>
2025-07-12 22:39:59 +0200

    add home page

--- Northwind.OData.Client.Mvc/Controllers/HomeController.cs
+++ Northwind.OData.Client.Mvc/Controllers/HomeController.cs
@@ -7,14 +7,31 @@ namespace Northwind.OData.Client.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory httpClientFactory;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            this.httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string startsWith = "Cha")
         {
+            try
+            {
+                HttpClient client = httpClientFactory.CreateClient("NorthWind.OData");
+                HttpRequestMessage request = new (                    
+                    method: HttpMethod.Get,
+                    requestUri: $"catalog/products/?$filter=startswith(ProductName, '{startsWith}')&$select=ProductId,ProductName,UnitPrice");
+                HttpResponseMessage response = await client.SendAsync(request);
+                ViewData["startsWith"] = startsWith;
+                ViewData["products"] = (await response.Content.ReadFromJsonAsync<ODataProducts>())?.Value;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error fetching products from OData service.");
+                ViewData["error"] = "An error occurred while fetching products.";
+            }
             return View();
         }
 
